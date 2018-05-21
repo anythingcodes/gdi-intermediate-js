443d203b6f65e8b53125c93e0e6b42852922b6bc
Liz Shaw <liz.h.shaw@gmail.com>
2018-05-21 12:08:21 -0400

    Wrap debugging activity in closure

--- activities/debugging/js/scripts.js
+++ activities/debugging/js/scripts.js
@@ -1,23 +1,30 @@
-// Instantiate variables
-var myNumber = 20;
-var totalFunctionsCalled = 0;
-var myArray = ['Ada', 'Aja'];
+// Wrap our code in a closure to move it from the global scope.
+// This is a best practice because it makes it easier for us to
+// monitor changes and prevents our variables from being overwritten :)
 
-function firstFunction() {
-  // Increment (add one to) totalFunctionsCalled
-  totalFunctionsCalled++;
-  // Push 'Hannah' onto array
-  myArray.push('Hannah');
-  // Call secondFunction
-  secondFunction();
-}
+(function () {
+  // Instantiate variables
+  var myNumber = 20;
+  var totalFunctionsCalled = 0;
+  var myArray = ['Ada', 'Aja'];
 
-function secondFunction() {
-  // Increment totalFunctionsCalled
-  totalFunctionsCalled++;
-  // Push 'Bob' onto array
-  myArray.push('Bob');
-}
+  function firstFunction() {
+    // Increment (add one to) totalFunctionsCalled
+    totalFunctionsCalled++;
+    // Push 'Hannah' onto array
+    myArray.push('Hannah');
+    // Call secondFunction
+    secondFunction();
+  }
 
-// Call firstFunction to start the chain of events
-firstFunction();
+  function secondFunction() {
+    // Increment totalFunctionsCalled
+    totalFunctionsCalled++;
+    // Push 'Bob' onto array
+    myArray.push('Bob');
+  }
+
+  // Call firstFunction to start the chain of events
+  firstFunction();
+})();
+
